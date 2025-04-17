b37112c9b4061f64cb4a7e1e686fc586bbe1c7a1
trevorlietch <123122931+trevorlietch@users.noreply.github.com>
2025-04-17 15:19:29 -0500

    Initial commit of main.py
    Has GUI for first page

--- main.py
+++ main.py
@@ -1,1 +1,48 @@
-print("poo poo pee pee, we're taking comp security!!!")
+import tkinter as tk
+from tkinter import messagebox
+
+class chatLogin():
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Secure Chat Login")
+        self.root.geometry("400x250")
+        self.root.resizable(False, False)
+        # Background color
+        self.root.configure(bg="#f0f0f0")
+
+        # Welcome label
+        tk.Label(
+            self.root,
+            text="Welcome to Secure Chat",
+            font=("Arial", 16, "bold"),
+            bg="#f0f0f0"
+        ).pack(pady=(40, 20))
+
+        # Password Prompt label
+        tk.Label(
+            self.root,
+            text="Enter Shared Password to Join a Private Chat",
+            font=("Arial", 12),
+            bg="#f0f0f0"
+        ).pack(pady=(50, 20))
+
+        # Password entry
+        self.password_entry = tk.Entry(
+            self.root, show="*", width=30,
+            font=("Arial", 12), bd=2, relief=tk.GROOVE
+        )
+        self.password_entry.pack(pady=5)
+
+        enterButton = tk.Button(
+            text = "ENTER",
+            bg = "#4CAF50",
+            fg = "white",
+            font=("Arial", 12, "bold"),
+            width=6
+        )
+        enterButton.pack(pady=5)
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = chatLogin(root)
+    root.mainloop()
